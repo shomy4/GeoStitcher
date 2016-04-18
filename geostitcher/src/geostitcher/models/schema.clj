b1f1d329b843c22f77b2ae908adede8997cc9a4d
root <shomy4@gmail.com>
2016-04-18 17:11:08 +0200

    Corrected users table creation function

--- geostitcher/src/geostitcher/models/schema.clj
+++ geostitcher/src/geostitcher/models/schema.clj
@@ -5,11 +5,11 @@
 (defn create-users-table []
   (sql/db-do-commands db
      (sql/create-table-ddl :users
-                            [:username    "VARCHAR(50) PRIMARY KEY"
+                            {:username    "VARCHAR(50) PRIMARY KEY"
                              :password    "VARCHAR(100)"
                              :first_name  "VARCHAR(50)"
                              :last_name   "VARCHAR(50)"
                              :occupation  "VARCHAR(50)"
                              :place       "VARCHAR(50)"
-                             :country     "VARCHAR(50)"])))
+                             :country     "VARCHAR(50)"})))
 
